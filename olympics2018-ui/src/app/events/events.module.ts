f99782dec14e7bcf2bf393421359786c581762b8
Matthew Timoney <mtimoney@college.harvard.edu>
2018-01-31 23:36:54 -0500

    convert event page to angular material

--- olympics2018-ui/src/app/events/events.module.ts
+++ olympics2018-ui/src/app/events/events.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { EventsComponent } from './events.component';
 import { EventsRoutingModule } from './events-routing.module';
 import { EventParticipantsComponent } from '../event-participants/event-participants.component'
 
 @NgModule({
-  imports: [CommonModule, MatRadioModule, EventsRoutingModule, FormsModule, ReactiveFormsModule, MatFormFieldModule],
+  imports: [
+    CommonModule, 
+    MatRadioModule, 
+    EventsRoutingModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatFormFieldModule,
+    MatDividerModule,
+    MatCardModule
+  ],
   declarations: [EventsComponent, EventParticipantsComponent],
   exports: [EventsComponent]
 })
